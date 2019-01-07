b8526f3e77c6aafd04c7d0756106ed3c3ac0ce6b
Sonu <sonu@ranium.in>
2019-01-07 14:53:37 +0530

    Store question data to database and display success message.

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -9,28 +9,27 @@
                     <div class="d-flex align-items-center">
                         <h2>Create Question</h2>
                         <a href="{{ route('questions.index') }}" class="btn btn-secondary">Back to all questions.</a>
-
                     </div>
                 </div>
                 <div class="panel-body">
                     <form action="{{ route('questions.store') }}" method="POST" >
-                        @csrf
-                        <div class="form-group">
+                        {{ csrf_field() }}
+                        <div class="form-group {{ $errors->has('title') ? 'has-error' : '' }} ">
                             <label for="question-title">Question Title.</label>
-                            <input type="text" name="title" id="title" value="question-title" placeholder="Enter Title" class="form-control {{ $errors->first('title') ? 'is-invalid' : '' }}">
+                            <input type="text" name="title" id="title" value="{{ old('title') }}" placeholder="Enter Title" class="form-control ">
                             @if ($errors->has('title'))
-                                <div class="invalid-feedback">
+                                <div class="help-block">
                                     <strong>{{ $errors->first('title') }}</strong>
                                 </div>
                             @endif
                         </div>
-                        <div class="form-group">
+                        <div class="form-group {{ $errors->has('body') ? 'has-error' : '' }}">
                             <label for="question-body">Explain your question.</label>
-                            <textarea  name="body" id="body" placeholder="" class="form-control" rows="10" {{ $errors->first('body') ? 'is-invalid' : '' }}>
-
+                            <textarea  name="body" id="body" class="form-control" rows="10" >
+                                {{ old('body') }}
                             </textarea>
                             @if ($errors->has('body'))
-                                <div class="invalid-feedback">
+                                <div class="help-block">
                                     <strong>{{ $errors->first('body') }}</strong>
                                 </div>
                             @endif
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -13,6 +13,8 @@
                     </div>
                 </div>
                 <div class="panel-body">
+                    @include('layouts._messages')
+
                     @foreach($questions as $question)
                         <div class="media">
                             <div class="d-flex flex-column counters">
